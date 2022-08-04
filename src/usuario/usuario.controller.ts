4045cae142dcc26aa2112107ee792aec4a3441f5
jpnc1695 <joaopaulonunescosta@gmail.com>
2022-08-04 16:38:26 -0300

    criaçai de metodo busca por nome de usuario

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,11 +1,18 @@
 import { Usuario } from './usuario.entity';
 import { UsuarioService } from './usuario.service';
-import { Body, Controller, Post } from "@nestjs/common"
+import { Body, Controller, Get, Post, Param } from "@nestjs/common"
 
 @Controller('users')
 export class UsuarioController {
   
   constructor(private usuarioService:UsuarioService){}
+
+
+  @Get(':nomeDeUsuairo')
+  public buscaUsuarioPorNome(@Param('nomeDeUsuairo') nomeDeUsuario:string):Usuario{
+    const usuarioAchado = this.usuarioService.buscaUsuarioPorNome(nomeDeUsuario)
+    return usuarioAchado
+  }
 
   @Post()
    public cria(@Body() usuario:Usuario):Usuario{
--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -4,7 +4,21 @@ import { Usuario } from "./usuario.entity";
 @Injectable()
 export class UsuarioService {
 
-  private usuarios = []
+  private usuarios = [
+    {
+      "id": 1,
+      "nomeDeUsuairo": "zizou",
+      "email": "email@email.com",
+      "senha": "123456",
+      "nomeCompleto": "Zinedine Zidane",
+      "dataDeEntrada": new Date()
+    }
+  ]
+
+  public buscaUsuarioPorNome(nomeDeUsuario:string):Usuario{
+    return this.usuarios.find(usuario => usuario.nomeDeUsuairo == nomeDeUsuario)
+
+  }
 
    public cria(usuario:Usuario):Usuario {
        this.usuarios.push(usuario);
