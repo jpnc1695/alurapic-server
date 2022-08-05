0863272aea1e152139cba95dbdb08f2ab8a55cb2
jpnc1695 <joaopaulonunescosta@gmail.com>
2022-08-05 14:39:03 -0300

    usso do class-validator na classe usuario

--- src/usuario/usuario.entity.ts
+++ src/usuario/usuario.entity.ts
@@ -1,9 +1,19 @@
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 export class Usuario {
   id:number;
+
+  @IsNotEmpty({message:'O campo nomeDeUsuario é obrigatorio'})
+  @IsString({message: 'Apenas strings são permitidas'})
   nomeDeUsuairo:string;
+
+  @IsEmail({message:'Formato de email email@email.com'})
   email:string;
+
+  @IsNotEmpty({message:'O campo senha é obrigatorio'})
   senha:string;
+
+  @IsNotEmpty({message:'O campo nomeCompleto é obrigatorio'})
   nomeCompleto:string;
   dataDeEntrada:Date;
 }
